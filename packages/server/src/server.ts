db29c3fb95562b4ebeac19ceb666989dd7b55f59
mkinfrared <mkinfrared@gmail.com>
2019-06-16 20:43:56 +1000

    feat(#002): Removed extra logging
    * Deleted extra logging

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -17,9 +17,6 @@ export let server: Server;
     console.log("Could not connect to database");
     console.log("Could not connect to database");
     console.log("Could not connect to database");
-    console.log("Could not connect to database");
-    console.log("Could not connect to database");
-    console.log("Could not connect to database");
     server = app.listen(SERVER_PORT, () =>
       console.log(`Server is running in port ${SERVER_PORT}`)
     );
