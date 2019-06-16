0c98fd5813301b395535ee522a7cae26bfef3c1f
mkinfrared <mkinfrared@gmail.com>
2019-06-16 19:36:10 +1000

    feat(Project): Project Setup
    * added husky
    * added lerna
    * added stylelint

---
+++ packages/server/src/server.ts
@@ -0,0 +1,22 @@
+import { SERVER_PORT } from "@util/secrets";
+import { Server } from "http";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+import app from "./app";
+
+export let server: Server;
+
+(async () => {
+  try {
+    await createConnection();
+    server = app.listen(SERVER_PORT, () =>
+      console.log(`Server is running in port ${SERVER_PORT}`)
+    );
+  } catch (e) {
+    console.log("Could not connect to database");
+    server = app.listen(SERVER_PORT, () =>
+      console.log(`Server is running in port ${SERVER_PORT}`)
+    );
+  }
+})();
